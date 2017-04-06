19bfec06a4193844c53a9e1afc38047d7785fb20
Chris McKee <cmckee41@qub.ac.uk>
2017-04-06 21:27:01 +0100

    Fixed minor bugs and added unit tests to prevent

--- src/board.rs
+++ src/board.rs
@@ -181,11 +181,10 @@ impl Board {
             self.bonus_cards = generate_bonus_stack(self.high_card);
             self.bonus_cards.pop()
         } else {
-            let temp = self.basic_cards.pop();
             if self.basic_cards.len() == 0 {
                 self.basic_cards = generate_basic_stack();
             }
-            temp
+            self.basic_cards.pop()
         };
         self.next_card = new_tile.unwrap();
     }
@@ -235,12 +234,96 @@ fn generate_basic_stack() -> Vec<u32> {
 fn generate_bonus_stack(high_card: u32) -> Vec<u32> {
     let mut stack: Vec<u32> = Vec::new();
     let mut next_value = high_card / 8;
-    while next_value != 3 {
-        println!("I'm a filthy shitburglar");
+    while next_value > 3 {
         stack.push(next_value);
         next_value = next_value / 2;
+        println!("{:?}", stack);
     }
     let mut rng = thread_rng();
     rng.shuffle(&mut stack);
     stack
 }
+
+#[test]
+fn test_collision_x_zero() {
+    let (x, y) = (0, 3);
+    let result = handle_collisions(x, y);
+    assert_eq!(result, Some(y));
+}
+
+#[test]
+fn test_collision_one_two() {
+    let (x, y) = (1, 2);
+    assert_eq!(handle_collisions(x, y), Some(3));
+    assert_eq!(handle_collisions(y, x), Some(3));
+}
+
+#[test]
+fn test_collision_of_like_values() {
+    assert_eq!(handle_collisions(3, 3), Some(6));
+    assert_eq!(handle_collisions(6, 6), Some(12));
+    assert_eq!(handle_collisions(12, 12), Some(24));
+    assert_eq!(handle_collisions(24, 24), Some(48));
+    assert_eq!(handle_collisions(48, 48), Some(96));
+    assert_eq!(handle_collisions(96, 96), Some(192));
+    assert_eq!(handle_collisions(192, 192), Some(384));
+}
+
+#[test]
+fn test_collision_fails() {
+    assert_eq!(handle_collisions(1, 1), None);
+    assert_eq!(handle_collisions(2, 2), None);
+    assert_eq!(handle_collisions(3, 1), None);
+    assert_eq!(handle_collisions(3, 2), None);
+    assert_eq!(handle_collisions(1, 3), None);
+    assert_eq!(handle_collisions(2, 3), None);
+}
+
+#[test]
+fn test_basic_stack() {
+    let mut stack = generate_basic_stack();
+    assert_eq!(12, stack.len());
+    stack.sort();
+    assert_eq!(stack[0], 1);
+    assert_eq!(stack[4], 2);
+    assert_eq!(stack[8], 3);
+
+}
+
+#[test]
+fn test_bonus_stack_empty() {
+    let stack = generate_bonus_stack(24);
+    assert_eq!(0, stack.len());
+}
+
+#[test]
+fn test_bonus_stack_96() {
+    let mut stack = generate_bonus_stack(96);
+    assert_eq!(2, stack.len());
+    stack.sort();
+    assert_eq!(stack[0], 6);
+    assert_eq!(stack[1], 12);
+}
+
+#[test]
+fn test_bonus_stack_192() {
+    let mut stack = generate_bonus_stack(192);
+    assert_eq!(3, stack.len());
+    stack.sort();
+    assert_eq!(stack[0], 6);
+    assert_eq!(stack[1], 12);
+    assert_eq!(stack[2], 24);
+}
+
+#[test]
+fn test_bonus_stack_384() {
+    let mut stack = generate_bonus_stack(384);
+    assert_eq!(4, stack.len());
+    stack.sort();
+    assert_eq!(stack[0], 6);
+    assert_eq!(stack[1], 12);
+    assert_eq!(stack[2], 24);
+    assert_eq!(stack[3], 48);
+}
+
+
